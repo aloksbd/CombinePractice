a3307c988db74d72464728edaee1d936e8a69a42
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-10-10 13:12:20 +0545

    create tableView to show employees

--- CombinePractice/ViewController.swift
+++ CombinePractice/ViewController.swift
@@ -13,12 +13,27 @@ extension Notification.Name {
     static let getEmployees = Notification.Name("get_employees")
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        employees.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = employees[indexPath.row].name
+        cell.detailTextLabel?.text = "salary: $\(employees[indexPath.row].salary)\t age: \(employees[indexPath.row].age)"
+        return cell
+    }
+    
     private var employees = [Employee](){
         didSet{
-            print(employees)
+            DispatchQueue.main.async {[weak self] in
+                
+                self?.tableView.reloadData()
+            }
         }
     }
+    @IBOutlet weak var tableView: UITableView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
